1afb79f993733b199155553b875d7cc2e3d50cc7
rodrigo-save <rodrigo.save2@gmail.com>
2023-04-06 16:28:52 -0300

    introdução ao js

---
+++ aula_javascript_rodrigo/js/seletores.js
@@ -0,0 +1,14 @@
+let nameAmiga = document.getElementById("amiga")
+nameAmiga.textContent= "Renata"
+
+console.log(nameAmiga)
+
+let nameFrase = document.getElementById('frase')
+nameFrase.textContent= "A vitoria só sorri para aquele que persiste"
+console.log(nameFrase)
+
+let frase = "A vitoria só sorri para aquele que persiste "
+
+let nameFoco = document.getElementById('foco')
+nameFoco.textContent= frase + "e nunca desiste"
+
